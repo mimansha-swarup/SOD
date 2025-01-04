e7805f6d13d229e2cc25d8f5be9a76adca1ec383
mimansha-swarup <mimanshaswarup@gmail.com>
2025-01-04 21:51:47 +0530

    refactor: streamline component structure and enhance UI consistency; update layout for improved user experience across ConfigurePage, AddMetricSheet, and TrackerList

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div className="h-screen w-screen bg-background flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Work In Progress</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
