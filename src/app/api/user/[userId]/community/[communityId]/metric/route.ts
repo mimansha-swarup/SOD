efa0df6f00ffc4f6aba32fc20ceba1325978b577
mimansha-swarup <mimanshaswarup@gmail.com>
2025-01-23 08:46:55 +0530

    refactor: comment out unused UPDATE functions in metric API routes to improve code clarity and maintainability

--- src/app/api/user/[userId]/community/[communityId]/metric/route.ts
+++ src/app/api/user/[userId]/community/[communityId]/metric/route.ts
@@ -60,35 +60,35 @@ export async function POST(req: NextRequest, { params }: contextType) {
     return Response.json({ message: "Something went wrong" }, { status: 500 });
   }
 }
-// Update Metric
-export async function UPDATE(req: NextRequest, { params }: contextType) {
-  try {
-    const { userId, communityId } = await params;
-    const searchParams = req.nextUrl.searchParams;
-    const metricId = searchParams.get("id");
+// // Update Metric
+// export async function UPDATE(req: NextRequest, { params }: contextType) {
+//   try {
+//     const { userId, communityId } = await params;
+//     const searchParams = req.nextUrl.searchParams;
+//     const metricId = searchParams.get("id");
 
-    const body = await req.json();
+//     const body = await req.json();
 
-    const userDocRef = doc(db, FIREBASE_COLLECTION.USERS, `${userId}`);
-    const userMetricDocRef = doc(
-      collection(userDocRef, FIREBASE_COLLECTION.METRICS),
-      communityId
-    );
-    const snapshot = await getDoc(userMetricDocRef);
-    const userMetric = snapshot.data() ;
+//     const userDocRef = doc(db, FIREBASE_COLLECTION.USERS, `${userId}`);
+//     const userMetricDocRef = doc(
+//       collection(userDocRef, FIREBASE_COLLECTION.METRICS),
+//       communityId
+//     );
+//     const snapshot = await getDoc(userMetricDocRef);
+//     const userMetric = snapshot.data() ;
 
-    if (userMetric) {
-      userMetric.metrics = userMetric.metrics?.map((metric) =>
-        metric.id === metricId ? body : metric
-      );
+//     if (userMetric) {
+//       userMetric.metrics = userMetric.metrics?.map((metric) =>
+//         metric.id === metricId ? body : metric
+//       );
 
-      await updateDoc(userMetricDocRef, userMetric);
+//       await updateDoc(userMetricDocRef, userMetric);
 
-      return Response.json({ data: userMetric }, { status: 200 });
-    }
+//       return Response.json({ data: userMetric }, { status: 200 });
+//     }
 
-    return Response.json({ message: "Metric not found" }, { status: 404 });
-  } catch (error) {
-    return Response.json({ message: "Something went wrong" }, { status: 500 });
-  }
-}
+//     return Response.json({ message: "Metric not found" }, { status: 404 });
+//   } catch (error) {
+//     return Response.json({ message: "Something went wrong" }, { status: 500 });
+//   }
+// }
